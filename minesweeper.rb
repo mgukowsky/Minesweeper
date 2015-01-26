1f2e5c09add70fa42f1791666dc45687fa0e83f7
= <=>
2015-01-26 12:17:10 -0500

    add neighbor bomb count

--- minesweeper.rb
+++ minesweeper.rb
@@ -14,6 +14,15 @@ class Tile
 end
 
 class Board
+  DIRECTIONS = [[0, 1],
+                [1, 1],
+                [1, 0],
+                [1, -1],
+                [0, -1],
+                [-1, -1],
+                [-1, 0],
+                [-1, 1]]
+
   attr_reader :tiles
 
 
@@ -28,10 +37,6 @@ class Board
       @tiles << tile_row
     end
   end
-
-
-
-
 
   def seed_bombs
     bomb_count = 0
@@ -54,7 +59,30 @@ class Board
 
   def [] (x,y)
     self.tiles[x][y]
+  end
 
+  def flagged(x, y)
+    self[x, y].flagged = true
+  end
+
+  def revealed(x, y)
+    self[x, y].revealed = true
+  end
+
+  def neighbor_bomb_count(x,y)
+    neighbor_bombs = 0
+    DIRECTIONS.each do |direction|
+      x_diff = direction[0]
+      y_diff = direction[1]
+      next unless x+x_diff.between(-8,8)
+      next unless y+y_diff.between(-8,8)
+      neighbor = self[x+x_diff,y+y_diff]
+      if neighbor.has_bomb
+        neighbor_bombs += 1
+      end
+    end
+
+    neighbor_bombs
 
   end
 
